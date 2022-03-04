d5a544e8b07f16dbb906aa82d49b002aadbcafe5
Sebastian Muzalewski <paulmuza0531@gmail.com>
2022-03-04 17:23:17 -0500

    Added Filter

--- src/ClothingList/ClothingList.js
+++ src/ClothingList/ClothingList.js
@@ -4,7 +4,7 @@ import {useNavigate} from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const [filter, setFilter] = useState("");
+
 
 const defaultclothingList = [
   { id: "1", name: "BucketHat", price: "$20.00",description: "Round shape hat", stock: 2},
@@ -14,9 +14,11 @@ const defaultclothingList = [
   { id: "5", name: "Jacket", price: "$25.00",description: "Puffy jacket", stock: 4},
 ];
 
-// const filteredList = defaultclothingList.filter(ClothingItem => ClothingItem.name.startsWith(filter));
+
 
 export const ClothingList = () => {
+  const [filter, setFilter] = useState("");
+  const filteredList = defaultclothingList.filter(ClothingItem => ClothingItem.name.startsWith(filter));
   console.log('ClothingList rendered');
   const navigate = useNavigate();
   return (
@@ -44,7 +46,34 @@ export const ClothingList = () => {
         </tbody>
       </table>
       <Button onClick={() => navigate("/")} variant="primary" >Login</Button><br></br>
-      
+      <label> Filter: </label> 
+      <input
+        type="text"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}>
+      </input>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>price</th>
+            <th>description</th>
+            <th>stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredList.map(({id, name, price, description, stock}, index) => (
+            <tr key={index}>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{price}</td>
+              <td>{description}</td>
+              <td>{stock}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
     
   );
@@ -55,19 +84,3 @@ export const ClothingList = () => {
 
 
 
-// const addItemToStock = (item) => {
-// 
-//   const existingItem = defaultclothingList.find((clothesItem) => clothesItem.name === item.name)
-//   if (existingItem) {
-//     const newClothes = [...defaultclothingList];
-//     const item = newClothes.find((clothesItem) => clothesItem.name === item.name)
-//     item.stock += item.stock
-//     setDefaultclothingList(newClothes)
-//   }
-//   setDefaultclothingList([...defaultclothingList, item]) 
-// }
-
-// const items = (name, price, descript, stock) => {
-//   const item = [{id: ClothingList.length, name: name, price: price, description: descript, stock: stock}]
-//   addItemToStock(item)
-// }dasdasdsad
